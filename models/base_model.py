86eb0910fcf8b691549235fb1ba30889f9013be9
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-01-13 22:46:35 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,32 @@ Module for the BaseModel class that defines common attributes/methods for other 
 
 from datetime import datetime
 import uuid
+from models import storage
 
 class BaseModel:
     """
     BaseModel class that defines common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Unused positional arguments.
+            **kwargs: Dictionary containing attribute names and values.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -30,6 +43,7 @@ class BaseModel:
         Updates the public instance attribute updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -40,21 +54,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-# Test the BaseModel implementation
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-
-    print(my_model)
-
-    my_model.save()
-    print(my_model)
-
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
